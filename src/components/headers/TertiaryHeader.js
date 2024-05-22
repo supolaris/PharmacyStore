6173b8643f58cfcf5f8d1ef23a7861e220c13d73
Muhammad Suleman <msulaman061@gmail.com>
2024-05-22 10:23:19 +0500

    product number in cart added

--- src/components/headers/TertiaryHeader.js
+++ src/components/headers/TertiaryHeader.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {View, Text, StyleSheet, Pressable, Image} from 'react-native';
 
 import {PharmacyAppColors} from '../../colors/Colors';
 
 import ArrowIcon from 'react-native-vector-icons/AntDesign';
 import SearchIcon from 'react-native-vector-icons/AntDesign';
+import {useFocusEffect} from '@react-navigation/native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const TertiaryHeader = props => {
+  const [productNumber, setProductNumber] = useState();
+
+  useFocusEffect(
+    useCallback(() => {
+      getProductNumberInCart();
+    }, []),
+  );
+
+  let productNumbers;
+  const getProductNumberInCart = async () => {
+    productNumbers = await AsyncStorage.getItem('TotalNumberOfProductsInCart');
+    setProductNumber(productNumbers);
+  };
   return (
     <View style={styles.container}>
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
@@ -29,6 +45,20 @@ const TertiaryHeader = props => {
           <Pressable
             style={styles.cartPressable}
             onPress={props.onCartImagePressed}>
+            <Text
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 40,
+                backgroundColor: 'red',
+                borderRadius: 100,
+                paddingHorizontal: 5,
+                color: 'white',
+                flex: 1,
+              }}>
+              {productNumber}
+            </Text>
+
             <Image
               style={styles.cartImage}
               source={require('../../assests/images/cartVector.png')}
